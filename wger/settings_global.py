a603efdc2343378d14e1dc381babd9153dba2298
Edwin Kato <edwin.kato@andela.com>
2017-07-25 19:00:38 +0300

    Set static url root

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -310,8 +310,11 @@ THUMBNAIL_ALIASES = {
 #
 # Django compressor
 #
-STATIC_ROOT = ''
+
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-collected')
 
 # The default is not DEBUG, override if needed
 # COMPRESS_ENABLED = True
