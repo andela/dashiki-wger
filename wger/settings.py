831d834280fdb25c5ab882a94ea2523348ac2a74
Edwin Kato <edwin.kato@andela.com>
2017-07-25 23:13:32 +0300

    Modify settings.py to work with local settings aswell

--- wger/settings.py
+++ wger/settings.py
@@ -14,10 +14,19 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'wger',
+        'USER':     'postgres',
+        'PASSWORD': '',
+        'TEST': {'CHARSET': 'UTF8'}
+    }
+}
 
-DATABASES = {}
+# Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'] = db_from_env
+DATABASES['default'].update(db_from_env)
 
 
 # DATABASES['default'] =  dj_database_url.config(default='postgres://foo:bar@somehost.amazonaws.com:5432/somedb')
