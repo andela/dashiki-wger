e659dbed56b6aa9da93d4a74912b78adc9b89614
Edwin Kato <edwin.kato@andela.com>
2017-07-31 12:48:32 +0300

    Set settings file in wsgi.py

--- wger/wsgi.py
+++ wger/wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wger.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
