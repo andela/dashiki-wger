c49778a269f7553f006c4d8b0d44bf42bd36690c
Seremba John <john.seremba@andela.com>
2017-08-01 16:06:32 +0300

    [#149466921] Add toggle for active and inactive users

--- wger/gym/managers.py
+++ wger/gym/managers.py
@@ -33,8 +33,8 @@ class GymManager(models.Manager):
         perm_gym = Permission.objects.get(codename='manage_gym')
         perm_gyms = Permission.objects.get(codename='manage_gyms')
         perm_trainer = Permission.objects.get(codename='gym_trainer')
-
-        users = User.objects.filter(userprofile__gym_id=gym_pk, is_active=kwargs['active'] or False)
+        active = kwargs.get('active', True)
+        users = User.objects.filter(userprofile__gym_id=gym_pk, is_active=active)
         return users.exclude(Q(groups__permissions=perm_gym) |
                              Q(groups__permissions=perm_gyms) |
                              Q(groups__permissions=perm_trainer)).distinct()
