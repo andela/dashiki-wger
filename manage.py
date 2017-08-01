5bfd707883792b7e1373b77d676f172d64df06fc
Edwin <katedwin88@gmail.com>
2017-08-01 13:22:29 +0300

    [#149468479] Ch set up heroku (#1)
    * Add settings setup to heroku
    
    * Uncomment out settings.py from .gitignore
    
    * Switch settings to use default django_settings_module
    
    * Set static url root
    
    * Remove invoke command from Procfile
    
    * Set settings variables to check for environment variables first
    
    * Change gunicorn to point to wger.wsgi
    
    * Create environment specific db configurations
    
    * Add gunicorn to requirements file
    
    * Add psycopg2 to the requirements file
    
    * Add migrations
    
    * Modify settings.py to work with local settings aswell
    
    * Add whitenoise configuration
    
    * Set static_url to be and environment variable
    
    * Rectify static url setting
    
    * Add BOWER_COMPONENTS_ROOT to settings file
    
    * Add bower post install to package.json
    
    * Add node version to package.json
    
    * Rectify bower postinstall script
    
    * Add bower.json configuration
    
    * Remove committed settings.py file
    
    * Add settings.py to gitignore
    
    * Add generation of create_settings to Procfile
    
    * Add bower components folder to eslint ignore
    
    * Add settings file location to travis configuration
    
    * Add create settings command to the release phase of heroku
    
    * Add settings.py to pep8 exclusions
    
    * Set settings file in wsgi.py
    
    * Add create_settings
    
    * Move bootstraping application to release phase
    
    * Change settings path
    
    * Revert to previous settings path
    
    * Move invoke to custom build pack
    
    * Add bower components to .gitignore
    
    * Remove static components from remote repo

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import os
 
 from django.core.management import execute_from_command_line
 
@@ -11,10 +12,10 @@ from tasks import (
 if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
-    if not any('--settings' in s for s in sys.argv):
-        setup_django_environment(get_user_config_path('wger', 'settings.py'))
+    # if not any('--settings' in s for s in sys.argv):
+    #     setup_django_environment(get_user_config_path('wger', 'settings.py'))
 
     # Alternative to above
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wger.settings")
 
     execute_from_command_line(sys.argv)
