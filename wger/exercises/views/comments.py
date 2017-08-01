dd1bad411b916c45c3b79a9fb041617e0e8a5f4d
Edwin Kato <edwin.kato@andela.com>
2017-08-01 17:18:13 +0300

    Rectify flake8 errors in exercises views

--- wger/exercises/views/comments.py
+++ wger/exercises/views/comments.py
@@ -15,7 +15,8 @@
 # You should have received a copy of the GNU Affero General Public License
 import logging
 
-from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin,\
+    LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -51,11 +52,13 @@ class ExerciseCommentEditView(WgerFormMixin,
     permission_required = 'exercises.change_exercisecomment'
 
     def get_success_url(self):
-        return reverse('exercise:exercise:view', kwargs={'id': self.object.exercise.id})
+        return reverse('exercise:exercise:view',
+                       kwargs={'id': self.object.exercise.id})
 
     # Send some additional data to the template
     def get_context_data(self, **kwargs):
-        context = super(ExerciseCommentEditView, self).get_context_data(**kwargs)
+        context = super(
+            ExerciseCommentEditView, self).get_context_data(**kwargs)
         context['form_action'] = reverse('exercise:comment:edit',
                                          kwargs={'pk': self.object.id})
 
@@ -76,19 +79,23 @@ class ExerciseCommentAddView(WgerFormMixin,
     permission_required = 'exercises.add_exercisecomment'
 
     def form_valid(self, form):
-        form.instance.exercise = Exercise.objects.get(pk=self.kwargs['exercise_pk'])
+        form.instance.exercise =\
+            Exercise.objects.get(pk=self.kwargs['exercise_pk'])
         return super(ExerciseCommentAddView, self).form_valid(form)
 
     def get_success_url(self):
-        return reverse('exercise:exercise:view', kwargs={'id': self.object.exercise.id})
+        return reverse('exercise:exercise:view',
+                       kwargs={'id': self.object.exercise.id})
 
     def get_context_data(self, **kwargs):
         '''
         Send some additional data to the template
         '''
-        context = super(ExerciseCommentAddView, self).get_context_data(**kwargs)
-        context['form_action'] = reverse('exercise:comment:add',
-                                         kwargs={'exercise_pk': self.kwargs['exercise_pk']})
+        context = super(
+            ExerciseCommentAddView, self).get_context_data(**kwargs)
+        context['form_action'] =\
+            reverse('exercise:comment:add',
+                    kwargs={'exercise_pk': self.kwargs['exercise_pk']})
 
         return context
 
@@ -100,4 +107,5 @@ def delete(request, id):
     exercise_id = comment.exercise.id
     comment.delete()
 
-    return HttpResponseRedirect(reverse('exercise:exercise:view', kwargs={'id': exercise_id}))
+    return HttpResponseRedirect(reverse('exercise:exercise:view',
+                                        kwargs={'id': exercise_id}))
