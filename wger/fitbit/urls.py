cd627bf5cecb58b89ea353cc4df3258887f9378f
Edwin Kato <edwin.kato@andela.com>
2017-08-15 16:51:20 +0300

    [Feature #149466929] Change url naming for fitbit weights

--- wger/fitbit/urls.py
+++ wger/fitbit/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from wger.fitbit.views import (
-    ExercisesListView,
+    WeightsListView,
     FitbitAuthentication
 )
 
 urlpatterns = [
-    url(r'^exercises/$', ExercisesListView.as_view(), name='exercises'),
+    url(r'^fitbit_weights/$', WeightsListView.as_view(), name='weights'),
     url(r'^login/$', FitbitAuthentication.as_view(), name='login')
 ]
