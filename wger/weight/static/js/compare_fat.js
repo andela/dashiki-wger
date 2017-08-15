d177944bca1769375e7abac6f91112467d2c295a
John Paul Seremba <serembaj@gmail.com>
2017-08-16 00:09:23 +0300

    Ft compare members #149466939 (#14)
    * Add member compare url, compare members template, create GymUserCompare class
    
    * Add comparision checkboxes
    
    * [#149466939] Send context data according to users
    
    * [#149466939] Get individual user weight
    
    * Grant gym managers and gym trainers access to all weights. Create weights static file
    
    * Create api to get multiple weights for members, Draw multiline graph comparing member weights, Create API URL, Add Javascript functionality for getting mutliple member weights
    
    * [#149466939] Add Tabs for individual weights
    
    * [#149466939] Get users by keys
    
    * [#149466939] Add selection of multiple users handle user ids in the url
    
    * [#149466939] Remove redundant code
    
    * [#149466939] Pass user credentials to the weight.js file, modify get-multiple-weights url params
    
    * [#149466939] Create nutritional data api
    
    * [#149466939] Add parameters to api url
    
    * [#149466939] Draw energy graph
    
    * [#149466939] Draw fat intake graph
    
    * [#149466939] Draw protein intake graph
    
    * [#149466939] Draw Carbohydrates intake graph
    
    * [#149466939] Add Nutritional Data details
    
    * Remove console.log in the js files, Finalize comparision of user details
    
    * [#149466939] Fix pep8 issues in gym.py and weight/views.py
    
    * [#149466939] Fix gulp lint issues
    
    * [#149466939] Fix gulp lint issues
    
    * [#149466939] Fix gulp lint issues in compare_energery.js"
    
    * [#149466939] Add test helper functions for add user and add_weight
    
    * [#149466939] Add test for multiple weight api
    
    * [#149466939] Add test for compare weight
    
    * [#149466939] Fix pep8 issues in the test_compare_users test file
    
    * [#149466939] Add test for nutritional plans api
    
    * [#149466939] Add tests for weight data and weight overview
    
    * [#149466939] Ingore unecessary lines for tests
    
    * [#149466939] Remove single letter variables.Replace For loop with join in the js files.Remove unecessary nesting of if statements

---
+++ wger/weight/static/js/compare_fat.js
@@ -0,0 +1,84 @@
+/*
+ This file is part of wger Workout Manager.
+
+ wger Workout Manager is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ wger Workout Manager is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ */
+'use strict';
+
+function modifyTimePeriod(data, pastNumberDays) {
+  var filtered;
+  var date;
+  if (data.length && pastNumberDays !== 'all') {
+    date = new Date();
+    date.setDate(date.getDate() - pastNumberDays);
+    filtered = MG.clone(data).filter(function (value) {
+      return value.date >= date;
+    });
+    return filtered;
+  }
+  return data;
+}
+
+$(document).ready(function () {
+  var username;
+  var url;
+  var chartParams;
+  var weightChart;
+  weightChart = {};
+  chartParams = {
+    title: 'Fat Intake (g)',
+    animate_on_load: true,
+    full_width: true,
+    top: 10,
+    left: 50,
+    right: 20,
+    show_secondary_x_label: true,
+    xax_count: 10,
+    target: '#fat_diagram',
+    x_accessor: 'date',
+    y_accessor: 'fat',
+    min_y_from_data: true,
+    colors: ['#CCCCFF', '#3465a4', 'blue', 'rgb(255,100,43)']
+  };
+
+  username = $('#fat').data('currentUsername');
+  url = '/weight/api/get_nutritional_data/' + username;
+  d3.json(url, function (json) {
+    var data = [];
+    var legend = [];
+    Object.keys(json).forEach(function (key, index) {
+      data[index] = MG.convert.date(json[key], 'date');
+      legend.push(key);
+    });
+
+    if (data.length) {
+      weightChart.data = data;
+      chartParams.data = data;
+      chartParams.legend = legend;
+      chartParams.legend_target = '#fat_legend';
+      MG.data_graphic(chartParams);
+    }
+  });
+
+  $('.modify-time-period-controls button').click(function () {
+    var pastNumberDays = $(this).data('time_period');
+    var data = modifyTimePeriod(weightChart.data, pastNumberDays);
+
+    // change button state
+    $(this).addClass('active').siblings().removeClass('active');
+    if (data.length) {
+      chartParams.data = data;
+      MG.data_graphic(chartParams);
+    }
+  });
+});
